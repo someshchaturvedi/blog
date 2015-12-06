be520ab67bd803da8a1a2493abfc1755e3143d31
somesh <somesh.08.96@gmail.com>
2015-12-06 18:42:03 +0530

    blog displayed on template

--- cogni/views.py
+++ cogni/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render , get_object_or_404
+from .models import blog , comment
+from django.views import generic
 
-def index(request):
-  #blog = blog.objects.all
-  return render(request , 'cogni/index.html')
+class index(generic.ListView):
+  context_object_name = 'latest_blog_list'
+  template_name = 'cogni/index.html'
+
+  def get_queryset(self):
+    return blog.objects.order_by('-id')[:3]
+
+class DetailView(generic.DetailView):
+  model = blog
+  template_name = 'cogni/detail.view'
